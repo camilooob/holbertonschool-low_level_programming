ca584c005b7b838a8f633d53063508e69f41c4a5
camilooodev <camilobaq@hotmail.com>
2019-09-18 22:49:33 +0000

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");	
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
