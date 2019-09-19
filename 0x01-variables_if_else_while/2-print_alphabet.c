05e1bcb6b58cb1d2406088652d721194ad45b0dd
camilooodev <camilobaq@hotmail.com>
2019-09-19 22:58:45 +0000

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letra;
+for(letra=0;letra <= 'z';letra++)
+{
+putchar(letra);
+putchat(\n);
+}
+return (0);
+}
