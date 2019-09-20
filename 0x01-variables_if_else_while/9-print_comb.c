22bc1646303ab8eb54fdb1c71765c89496188fa8
camilooodev <camilobaq@hotmail.com>
2019-09-20 01:41:26 +0000

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numero;
+for (numero = '0'; numero <= '9'; numero++)
+{
+putchar(numero);
+if (numero != '9')
+{
+putchar(' ');
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
+}
