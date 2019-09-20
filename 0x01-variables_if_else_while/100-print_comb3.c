3e511e6f73f7bb0172f78836f148b328e59cd168
camilooodev <camilobaq@hotmail.com>
2019-09-20 18:19:48 +0000

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdioi.h>
+/*dvs
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48 ; i < 58 ; i++)
+{
+for (j = 48 ; j < 58 ; j++)
+{
+if ((i < j) && (i != j))
+{
+putchar(i);
+putchar(j);
+if ((i == 56) && (j == 57))
+{
+break;
+}
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
