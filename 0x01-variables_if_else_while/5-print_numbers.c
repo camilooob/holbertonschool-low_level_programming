231f98262eb4863497c14a4db1dddf43c2810381
camilooodev <camilobaq@hotmail.com>
2019-09-20 00:40:23 +0000

    55

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numero;
+for (numero = 0; numero <= 9; numero++)
+{
+printf("%i",numero);
+}
+printf("\n");
+return (0);
+}
