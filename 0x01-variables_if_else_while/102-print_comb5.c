51a1afe4c77a37f6cb85e9054cdadf6e8c428ceb
camilooodev <camilobaq@hotmail.com>
2019-09-21 03:11:53 +0000

    1022

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{/*main*/
+{
 int i, j, k, l;
 for (i = 48 ; i < 58 ; i++)
 {
@@ -28,11 +28,11 @@ break;
 }
 putchar(44);
 putchar(32);
-} /*if ((i == 57)...*/
-} /*for*/
-} /*for*/
-} /*for*/
-} /*for*/
+}
+}
+}
+}
+}
 putchar(10);
 return (0);
-} /*main*/
+}
