385d276bb40be58b1ecd5ba1c6569a19831250df
Camilo <camilobaq@hotmail.com>
2019-09-23 11:11:08 -0500

    start the day

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char h = 'H';
+char o = 'o';
+char l = 'l';
+char b = 'b';
+char e = 'e';
+char r = 'r';
+char t = 't';
+char n = 'n';
+putchar(h);
+putchar(o);
+putchar(l);
+putchar(b);
+putchar(e);
+putchar(r);
+putchar(t);
+putchar(o);
+putchar(n);
+putchar('\n');
+return (0);
+}
