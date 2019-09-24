c412c2f2cb6bce43a21a7a2b2cc03a22c568709f
camilooodev <camilobaq@hotmail.com>
2019-09-24 23:29:28 +0000

    999

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,52 +5,37 @@
  */
 void times_table(void)
 {
-int fila, columna, resultado, resultado1, resultado2;
-for (fila = 0; fila <= 9; fila++)
-{
-for (columna = 0; columna <= 9; columna++)
-{
-resultado = fila * columna;
-resultado1 = resultado / 10;
-resultado2 = resultado % 10;
-if (columna > 0 && columna < 9)
-{
-if (resultado <= 9)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + resultado2);
-_putchar(',');
-}
-else
-{
-_putchar(' ');
-_putchar('0' + resultado1);
-_putchar('0' + resultado2);
-_putchar(',');
-}
-}
-else if (columna == 0)
-{
-_putchar('0' + resultado2);
-_putchar(',');
-}
-else
-{
-if (resultado <= 9)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + resultado2);
-}
-else
-{
-_putchar(' ');
-_putchar('0' + resultado1);
-_putchar('0' + resultado2);
-}
-_putchar('\n');
-}
-}
-}
+	int num1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int num2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i, j;
+	int result;
+	int first_digit;
+	int last_digit;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = num1[i] * num2[j];
+			first_digit = result / 10;
+			last_digit = result % 10;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+					_putchar(' ');
+			}
+			if (result < 10)
+				_putchar('0' + result);
+			else
+			{
+				_putchar('0' + first_digit);
+				_putchar('0' + last_digit);
+			}
+		}
+		_putchar('\n');
+	}
+
+
 }
