1b50cfe63ef6fc2f339f1e77c09609a9e6afeb17
camilooob <camilobaq@hotmail.com>
2019-09-26 16:11:06 +0000

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ ** _isdigit - if is a digit between 1 a 9 show 1
+ ** @c: integer input
+ ** Return: 0 or 1
+ **/
+int _isdigit(int c)
+{
+if (c > 47 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
