e1e5f08a100048d2c001679560c29095026d078e
camilooob <camilobaq@hotmail.com>
2019-09-26 17:44:45 +0000

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ ** print_most_numbers - print numbers
+ ** Return: no return
+ **
+ **/
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+if (i != 50 && i != 52)
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
