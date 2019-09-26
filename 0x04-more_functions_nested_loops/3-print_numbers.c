8c73eb7c19732b2d4c545b5bc1c39ddf9cfa76c9
camilooob <camilobaq@hotmail.com>
2019-09-26 17:19:51 +0000

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+** print_numbers - write the numbers
+** Return: no return
+**/
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -9,3 +9,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char c);
