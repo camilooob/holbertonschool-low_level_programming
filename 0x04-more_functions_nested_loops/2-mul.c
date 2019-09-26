d23888770218a9f73b761fb59ce269b9308babd8
camilooob <camilobaq@hotmail.com>
2019-09-26 16:21:21 +0000

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ ** mul - multiply a and b
+ ** @a: input a
+ ** @b: input b
+ ** Return: result
+ **/
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
