67ee0d751560d232f9ff01dbcc03345e5075e4d3
camilooob <camilobaq@hotmail.com>
2019-09-27 00:58:15 +0000

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,20 @@
+#include "holberton.h"
 /**
  ** print_square - print a diagonal.
  ** @size: input diagonal.
  ** Return: no return.
  **/
-#include "holberton.h"
 void print_square(int size)
 {
 int columna;
 int fila;
 int numeral = 35;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
 for (columna = 1; columna <= size; columna++)
 {
 for (fila = 1; fila <= size; fila++)
@@ -16,5 +22,6 @@ for (fila = 1; fila <= size; fila++)
 _putchar(numeral);
 }
 _putchar('\n');
+}
 }
 }
