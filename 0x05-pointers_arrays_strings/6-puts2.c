d05e6aa3c67b010157a0f7b74e7d6dbf0515139f
Camilo <camilobaq@hotmail.com>
2019-10-01 12:59:03 -0500

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,17 +3,16 @@
  * put2 - a function that prints.
  * @str: input string
  */
-
 void puts2(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i % 2 == 0)
-		{
-		_putchar(str[i]);
-		}
-	}
-	_putchar('\n');
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
 }
