570adfb9f60d73700c8da260e6019722dd4df688
Camilo <camilobaq@hotmail.com>
2019-10-01 14:23:25 -0500

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * puts2 - a function that prints one char out of 2
+ * of a string, followed by a new line.
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+int i;
+int largo;
+largo = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+largo++;
+}
+if (largo %2 != 0)
+{
+for (i = (largo / 2) + 1; i < largo; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+for (i = largo / 2; i < largo; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
