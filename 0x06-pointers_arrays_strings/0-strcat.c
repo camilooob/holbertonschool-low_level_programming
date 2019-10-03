7b14f005e6efca979921c824c06ab6ea754593f1
Camilo <camilobaq@hotmail.com>
2019-10-03 11:46:29 -0500

    00

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _strcat - concatenates two words.
+ * @dest: output
+ * @src: input
+ * Return: x.
  */
 char *_strcat(char *dest, char *src)
 {
+	char *x = dest;
+
+	for (; *dest != '\0' ; dest++)
+	{
+		;
+	}
+	for (; *src != '\0'; src++)
+	{
+		*dest = *src;
+		dest++;
+	}
+	*dest = '\0';
+	return (x);
 }
