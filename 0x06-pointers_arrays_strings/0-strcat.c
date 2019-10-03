19b776c6122c4142a74331f7138e66868f86dd7f
Camilo <camilobaq@hotmail.com>
2019-10-03 10:51:55 -0500

    init files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+}
