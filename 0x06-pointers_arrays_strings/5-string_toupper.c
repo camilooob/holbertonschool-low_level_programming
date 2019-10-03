c27ed499a0e4e851dc8159a3404e76390baf0488
Camilo <camilobaq@hotmail.com>
2019-10-03 18:16:41 -0500

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ ** *string_toupper - put 1 when is a uppercase
+ ** @s: takes in an integer
+ ** Return: 1 if is uppercase and 0 if is lowcase
+ **/
+char *string_toupper(char *s)
+{
+int i;
+
+for (i = 0;s[i] != '\0';i++)
+{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+}
+return (s);
+}
+
