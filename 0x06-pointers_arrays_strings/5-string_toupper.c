b0b4ed36af79f756a6a4cbf3d4b2cf93af30c4b2
Camilo <camilobaq@hotmail.com>
2019-10-03 18:21:33 -0500

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,12 @@
 char *string_toupper(char *s)
 {
 int i;
-
-for (i = 0;s[i] != '\0';i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-		if (s[i] >= 97 && s[i] <= 122)
-		{
-			s[i] = s[i] - 32;
-		}
+if (s[i] >= 97 && s[i] <= 122)
+{
+s[i] = s[i] - 32;
+}
 }
 return (s);
 }
