1e9bdd3856a847620a9f87fa4ae3c9580361f29f
Camilo <camilobaq@hotmail.com>
2019-10-04 10:48:45 -0500

    100

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_number - print an integer.
+ * @n: input value§
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	unsigned int number;
+
+	number = n;
+	if (n < 0)
+	{
+		number = number * -1;
+		_putchar ('-');
+	}
+		if (n / 10)
+		{
+		print_number(number / 10);
+		}
+	_putchar(number % 10 + '0');
+}
