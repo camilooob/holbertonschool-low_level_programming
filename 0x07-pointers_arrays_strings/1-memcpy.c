bd47741011e298b4a719fd0aaca4a09dbabd79c9
Camilo <camilobaq@hotmail.com>
2019-10-07 14:49:00 -0500

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+
+#include "holberton.h"
+/**
+ * _memcpy - prints buffer in hexa
+ * @dest: output
+ * @src: source
+ * @n: integer
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+i = 0;
+	while (i < n)
+{
+	dest[i] = src[i];
+	i++;
+}
+return (dest);
+}
+
