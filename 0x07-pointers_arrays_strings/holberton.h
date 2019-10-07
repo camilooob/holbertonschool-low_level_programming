8aae75a1eac03aba7e9de3c996df99e187ae1b18
Camilo <camilobaq@hotmail.com>
2019-10-07 10:05:26 -0500

    00

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
