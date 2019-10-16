393c8b38d9d68a5651d5d3b4344d3e75a40e148b
Camilo <camilobaq@hotmail.com>
2019-10-15 20:19:52 -0500

    0fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,10 @@ char *create_array(unsigned int size, char c)
 char *a;
 unsigned int i;
 a = malloc(size * sizeof(c));
-if (a ==  NULL)
+if (a ==  NULL || size == 0)
 {
 return (0);
 }
-
-
 for (i = 0; i <= size ; i++)
 {
 	a[i] = c;
