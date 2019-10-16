f85416fb3f1c98365043ea682a936cf49db0f12c
Camilo <camilobaq@hotmail.com>
2019-10-15 21:19:27 -0500

    1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,4 +22,3 @@ for (i = 0; i <= size ; i++)
 }
 return (a);
 }
-
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _len - make array with malloc.
+ * @str: str.
+ * Return: Array.
+ */
+char _len(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{}
+	return (i);
+}
+/**
+ * _strdup - make array with malloc.
+ * @str: str
+ * Return: Array.
+ */
+char *_strdup(char *str)
+{
+char *a;
+int j = 0;
+int i = 0;
+j = _len(str);
+a = malloc(sizeof(str) * j);
+if (a ==  NULL || str == 0)
+{
+return (0);
+}
+
+for (i = 0; str[i] != '\0'; i++)
+{
+	a[i] = str[i];
+}
+return (a);
+}
