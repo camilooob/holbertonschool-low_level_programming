48b9f13cdeceea7609b829f893dac4f694321980
Camilo <camilobaq@hotmail.com>
2019-10-21 13:00:44 -0500

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts(__BASE_FILE__);
+	return (0);
+}
