47a9e8b7375894abea3cc74811f8a957cf9111d1
camilooob <camilobaq@hotmail.com>
2019-10-23 14:52:46 -0500

    2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,4 +5,5 @@ void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
