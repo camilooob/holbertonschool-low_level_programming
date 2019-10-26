76fdf457a3a8423dbc0bdc20dfda7ad814b2576b
camilooob <camilobaq@hotmail.com>
2019-10-26 13:14:13 -0500

    fix h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,11 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+/**
+ * struct select - create s.
+ * @q: input.
+ * @u: input.
+ *
+ * Return: int
+ *
+ **/
 typedef struct select
 {
 char q;
 void (*u)(va_list pa);
-}tstruc;
+} tstruc;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
