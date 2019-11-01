50e8101cfc883d13ff4879451cc77f7443caf4f9
camilooob <camilobaq@hotmail.com>
2019-10-31 19:55:35 -0500

    4-free

--- 0x12-singly_linked_lists/.history/4-free_list_20191031195405.c
+++ 0x12-singly_linked_lists/.history/4-free_list_20191031195405.c
@@ -7,10 +7,10 @@
  */
 void free_list(list_t *head)
 {
-	while (head->next != NULL)
-	{
-		free(head->str);
-		free(head);
-		head = head->next;
-	}
+	if (head == NULL)
+		return;
+	if (head->next != NULL)
+		free_list(head->next);
+	free(head->str);
+	free(head);
 }
