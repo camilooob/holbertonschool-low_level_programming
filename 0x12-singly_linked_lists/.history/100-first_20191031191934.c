1f8a9b6083ecf4f799f08d74eef26ef28a0a535f
camilooob <camilobaq@hotmail.com>
2019-10-31 19:21:15 -0500

    fix betty

--- 0x12-singly_linked_lists/.history/100-first_20191031191934.c
+++ 0x12-singly_linked_lists/.history/100-first_20191031191934.c
@@ -1,12 +1,8 @@
 #include "lists.h"
 void myStartupFun(void) __attribute__ ((constructor));
-/**
- * myStartupFun - start before main.
- * @void: input.
- * Return: text.
- */
+
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\n
+    printf ("You're beat! and yet, you must allow,\n
 	I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/.history/100-first_20191031192004.c
+++ 0x12-singly_linked_lists/.history/100-first_20191031192004.c
@@ -1,10 +1,6 @@
 #include "lists.h"
 void myStartupFun(void) __attribute__ ((constructor));
-/**
- * myStartupFun - start before main.
- * @void: input.
- * Return: text.
- */
+
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n
