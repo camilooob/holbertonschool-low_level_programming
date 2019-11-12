2d11eba920ad04e9c3e5509fec3f5ee01fff2c3c
camilooob <camilobaq@hotmail.com>
2019-11-12 14:48:56 -0500

    0

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
