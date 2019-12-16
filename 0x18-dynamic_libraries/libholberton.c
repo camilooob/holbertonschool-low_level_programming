e13238b8ba063deacff8bd1a433e5e146f51d6f5
camilooob <camilobaq@hotmail.com>
2019-12-16 12:53:25 -0500

    00

---
+++ 0x18-dynamic_libraries/libholberton.c
@@ -0,0 +1,81 @@
+#include "holberton.h"
+int _putchar(char c)
+{
+    return (0);
+}
+int _islower(int c)
+{
+    return (0);
+}
+int _isalpha(int c)
+{
+    return (0);
+}
+int _abs(int n)
+{
+    return (0);
+}
+int _isupper(int c)
+{
+    return (0);
+}
+int _isdigit(int c)
+{
+    return (0);
+}
+int _strlen(char *s)
+{
+    return (0);
+}
+void _puts(char *s)
+{
+
+}
+char *_strcpy(char *dest, char *src)
+{
+    return (0);
+}
+int _atoi(char *s)
+{
+    return (0);
+}
+char *_strcat(char *dest, char *src)
+{
+    return (0);
+}
+char *_strncat(char *dest, char *src, int n)
+{
+    return (0);
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+    return (0);
+}
+int _strcmp(char *s1, char *s2)
+{
+    return (0);
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+    return (0);
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    return (0);
+}
+char *_strchr(char *s, char c)
+{
+    return (0);
+}
+unsigned int _strspn(char *s, char *accept)
+{
+    return (0);
+}
+char *_strpbrk(char *s, char *accept)
+{
+    return (0);
+}
+char *_strstr(char *haystack, char *needle)
+{
+    return (0);
+}
