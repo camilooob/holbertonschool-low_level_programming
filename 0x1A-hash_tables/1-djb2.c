707c36e410d651d50201a4f33841e552d1dd5f3a
camilooob <camilobaq@hotmail.com>
2020-01-20 03:26:19 +0000

    fix descript 01

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementing djb2 algorithm
+ * @str: evaluate key
+ *
+ * Return: hash key
+ *
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
